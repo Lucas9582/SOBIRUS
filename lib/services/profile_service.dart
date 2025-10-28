92ac66ef15e6c800b24504e5324d5181e1a67826
Lucas9582 <159451671+Lucas9582@users.noreply.github.com>
2025-10-27 21:51:42 -0300

    arquivo 2/17

--- lib/services/profile_service.dart
+++ lib/services/profile_service.dart
@@ -1,41 +1,35 @@
-// lib/features/profile/data/services/profile_service.dart
-
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_storage/firebase_storage.dart';
 import 'dart:io';
 import 'package:sobrius_app/models/profile_model.dart';
+import 'package:sobrius_app/core/database/firebase_connection.dart'; // ADICIONAR
 import 'package:logger/logger.dart';
 
 final logger = Logger();
 
 class ProfileService {
-  final FirebaseFirestore _firestore = FirebaseFirestore.instance;
-  final FirebaseStorage _storage = FirebaseStorage.instance;
+  // ANTES: final FirebaseFirestore _firestore = FirebaseFirestore.instance;
+  // DEPOIS: Usar Singleton
+  final FirebaseFirestore _firestore = FirebaseConnection.getInstance().firestore;
+  final FirebaseStorage _storage = FirebaseConnection.getInstance().storage;
 
   final _profilesCollection = 'profiles';
 
-  /// Busca um perfil de usuário no Firestore.
-  /// Retorna um [ProfileModel] se o perfil for encontrado, ou null se não.
   Future<ProfileModel?> fetchProfile(String userId) async {
     try {
       final docSnapshot = await _firestore.collection(_profilesCollection).doc(userId).get();
       if (docSnapshot.exists) {
-        // CORREÇÃO: Usando o método fromMap do ProfileModel
         return ProfileModel.fromMap(docSnapshot.id, docSnapshot.data()!);
       }
       return null;
     } catch (e, s) {
-      // Em um aplicativo real, você logaria esse erro para depuração
       logger.e('Erro ao buscar perfil.', error: e, stackTrace: s);
       throw Exception('Falha ao buscar perfil.');
     }
   }
 
-  /// Salva ou atualiza um perfil de usuário no Firestore.
-  /// Usa `SetOptions(merge: true)` para evitar a substituição completa do documento.
   Future<void> saveProfile(ProfileModel profile) async {
     try {
-      // CORREÇÃO: Usando o método toMap do ProfileModel
       await _firestore.collection(_profilesCollection).doc(profile.id).set(profile.toMap(), SetOptions(merge: true));
     } catch (e, s) {
       logger.e('Erro ao salvar perfil.', error: e, stackTrace: s);
@@ -43,8 +37,6 @@ class ProfileService {
     }
   }
 
-  /// Faz o upload da imagem do perfil para o Firebase Storage.
-  /// Retorna a URL de download da imagem.
   Future<String> uploadProfileImage(String userId, File imageFile) async {
     try {
       final storageRef = _storage.ref().child('user_avatars/$userId.jpg');
@@ -58,7 +50,6 @@ class ProfileService {
     }
   }
 
-  /// Atualiza apenas a data de sobriedade de um perfil de usuário no Firestore.
   Future<void> updateSobrietyStartDate(String userId, DateTime newDate) async {
     try {
       await _firestore.collection(_profilesCollection).doc(userId).update({
