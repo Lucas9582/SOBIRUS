fc2cb6ea73fcbaa6842814b418f3db9dd407d9c3
Lucas9582 <159451671+Lucas9582@users.noreply.github.com>
2025-10-27 21:47:28 -0300

    arquivo 1/17 pronto

--- lib/core/database/firebase_connection.dart
+++ lib/core/database/firebase_connection.dart
@@ -1,1 +1,29 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_storage/firebase_storage.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
+class FirebaseConnection {
+  static FirebaseConnection? _instance;
+  
+  late final FirebaseFirestore _firestore;
+  late final FirebaseStorage _storage;
+  late final FirebaseAuth _auth;
+  
+  // Construtor privado (Singleton)
+  FirebaseConnection._internal() {
+    _firestore = FirebaseFirestore.instance;
+    _storage = FirebaseStorage.instance;
+    _auth = FirebaseAuth.instance;
+  }
+  
+  // Método para obter instância única
+  static FirebaseConnection getInstance() {
+    _instance ??= FirebaseConnection._internal();
+    return _instance!;
+  }
+  
+  // Getters
+  FirebaseFirestore get firestore => _firestore;
+  FirebaseStorage get storage => _storage;
+  FirebaseAuth get auth => _auth;
+}
